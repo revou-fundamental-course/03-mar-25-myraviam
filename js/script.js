ae819bbda2da9e095946580731e64da6c65089b6
Rafi <rafi28167@gmail.com>
2025-03-12 09:19:24 +0800

    update responsive layout

--- js/script.js
+++ js/script.js
@@ -3,3 +3,23 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
     header.classList.toggle("sticky", window.scrollY > 130);
 });
+
+let menu = document.querySelector('#menu-icon');
+let menulist = document.querySelector('.menulist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    menulist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    menulist.classList.remove('open');
+};
+
+var typed = new Typed(".input", {
+    strings:["Data Analyst.","Digital Marketing.","Software Engineer."],
+    typeSpeed: 120,
+    backSpeed: 70,
+    loop:true
+})
